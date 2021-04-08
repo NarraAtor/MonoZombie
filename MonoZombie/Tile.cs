586cb26ac79f9c51739d2b75edbe72164f32549e
Ken Adachi-Bartholomay <kennybart56@gmail.com>
2021-04-08 17:22:49 -0400

    Adjusted random values for different tile textures

--- MonoZombie/Tile.cs
+++ MonoZombie/Tile.cs
@@ -53,13 +53,37 @@ namespace MonoZombie {
 		private static Texture2D GetTexture (TileType tileType) {
 			// For generating a random index to get a random texture for the tile
 			Random random = new Random( );
-
+			int rng = random.Next(10);
 			// Based on what type the tile is, get a random texture from its corresponding array
 			switch (tileType) {
 				case TileType.Grass:
-					return Game1.grassTextures[random.Next(0, Game1.grassTextures.Length)];
+					if(rng < 8)
+                    {
+						return Game1.grassTextures[0];
+					}
+					else if(rng >= 8 && rng < 9)
+                    {
+						return Game1.grassTextures[1];
+					}
+                    else
+                    {
+						return Game1.grassTextures[2];
+					}
+
 				case TileType.Wall:
-					return Game1.wallTextures[random.Next(0, Game1.wallTextures.Length)];
+					if (rng < 8)
+					{
+						return Game1.wallTextures[0];
+					}
+					else if (rng >= 8 && rng < 9)
+					{
+						return Game1.wallTextures[1];
+					}
+					else
+					{
+						return Game1.wallTextures[2];
+					}
+
 				case TileType.Gravel:
 					return null;
 				case TileType.Lava:
